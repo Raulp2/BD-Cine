1e0b96c822cd17e7250a0686ef4dcc9ee286b56f
Raulp2 <raul.poza2@gmail.com>
2024-03-08 15:50:56 +0100

    Obtener html sin archivo, mas eficiente

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ filmaffinity = "https://www.filmaffinity.com/es/film932361.html"
 # Confirmar que somos humanos
 
 HEADERS = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-  "Accept": "*/*"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+    "Accept": "*/*"
 }
 
 # Donde guardamos el contenido 
@@ -15,11 +15,14 @@ def guardar(contenido: str, nombre: str ="res.html") -> None:
     with open(nombre, "w") as f:
         f.write(contenido)
 
-# Obtenciï¿½n y guardado del html del enlace
+# Obtención y guardado del html del enlace
 
 def obtener_html(url: str) -> str:
     res = requests.get(url, headers=HEADERS)
     return res.text
+    
 
 html = obtener_html(filmaffinity)
 guardar(html)
+
+
